687c58980447dbc57772e7bc9093e32b8bf13c31
Alimul Islam <alimulislam50@gmail.com>
2026-05-03 23:55:47 +0600

    feat: add build and SSG manifests for development
    - Introduced _buildManifest.js and _ssgManifest.js to manage build and static site generation metadata.
    - Added webpack hot update files to support live reloading during development.
    - Created layout type definitions to ensure type safety for layout components in the Next.js application.
    - Updated mobile submodule to the latest commit.

--- frontend/.next/server/middleware-build-manifest.js
+++ frontend/.next/server/middleware-build-manifest.js
@@ -1,1 +1,32 @@
-self.__BUILD_MANIFEST={polyfillFiles:["static/chunks/polyfills-42372ed130431b0a.js"],devFiles:[],ampDevFiles:[],lowPriorityFiles:[],rootMainFiles:["static/chunks/webpack-03f7c6bc932ce1e3.js","static/chunks/fd9d1056-d6d006985366b03a.js","static/chunks/117-e30939c35a11c3a4.js","static/chunks/main-app-5088420ed32c2997.js"],pages:{"/_app":["static/chunks/webpack-03f7c6bc932ce1e3.js","static/chunks/framework-f66176bb897dc684.js","static/chunks/main-67ebf269adab86fc.js","static/chunks/pages/_app-72b849fbd24ac258.js"],"/_error":["static/chunks/webpack-03f7c6bc932ce1e3.js","static/chunks/framework-f66176bb897dc684.js","static/chunks/main-67ebf269adab86fc.js","static/chunks/pages/_error-7ba65e1336b92748.js"]},ampFirstPages:[]},self.__BUILD_MANIFEST.lowPriorityFiles=["/static/"+process.env.__NEXT_BUILD_ID+"/_buildManifest.js",,"/static/"+process.env.__NEXT_BUILD_ID+"/_ssgManifest.js"];
+self.__BUILD_MANIFEST = {
+  "polyfillFiles": [
+    "static/chunks/polyfills.js"
+  ],
+  "devFiles": [
+    "static/chunks/react-refresh.js"
+  ],
+  "ampDevFiles": [],
+  "lowPriorityFiles": [],
+  "rootMainFiles": [
+    "static/chunks/webpack.js",
+    "static/chunks/main-app.js"
+  ],
+  "pages": {
+    "/_app": [
+      "static/chunks/webpack.js",
+      "static/chunks/main.js",
+      "static/chunks/pages/_app.js"
+    ],
+    "/_error": [
+      "static/chunks/webpack.js",
+      "static/chunks/main.js",
+      "static/chunks/pages/_error.js"
+    ]
+  },
+  "ampFirstPages": []
+};
+self.__BUILD_MANIFEST.lowPriorityFiles = [
+"/static/" + process.env.__NEXT_BUILD_ID + "/_buildManifest.js",
+,"/static/" + process.env.__NEXT_BUILD_ID + "/_ssgManifest.js",
+
+];
